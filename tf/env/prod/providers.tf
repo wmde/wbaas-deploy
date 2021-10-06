b3521694a09a27febf8cce0d8a3b46645956cde0
addshore <addshorewiki@gmail.com>
2021-10-06 10:33:18 +0100

    Remove helm provider from prod tf

--- tf/env/prod/providers.tf
+++ tf/env/prod/providers.tf
@@ -10,21 +10,10 @@ terraform {
       source = "hashicorp/kubernetes"
       version = "2.5.0"
     }
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.3.0"
-    }
   }
 }
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
     config_context = "gke_wikibase-cloud_europe-west3-a_wbaas-1"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-    config_context = "gke_wikibase-cloud_europe-west3-a_wbaas-1"
-  }
 }
