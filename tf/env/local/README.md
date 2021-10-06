68354de6b6c8fadf9c34d6184195547fdabcdbb7
addshore <addshorewiki@gmail.com>
2021-10-06 17:07:32 +0100

    Lets use minikube

---
+++ tf/env/local/README.md
@@ -0,0 +1,14 @@
+## Dev
+
+Install minikube
+
+Unfortunatly this can not be done using terraform at this time...
+
+In the local dir:
+
+```sh
+terraform init
+terraform apply
+```
+
+Everything should be green and not error, although nothing will actually be applied by terraform at this time.
--- tf/env/local/providers.tf
+++ tf/env/local/providers.tf
@@ -1,11 +1,11 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-    config_context = "kind-wbaas-local"
+    config_context = "minikube"
 }
 
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
-    config_context = "kind-wbaas-local"
+    config_context = "minikube"
   }
 }
