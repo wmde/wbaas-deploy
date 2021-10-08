981b5a5eba374123a0447800e59e1c8e1b44f62b
toan <tobias.andersson@wikimedia.de>
2021-10-08 18:28:25 +0200

    Continue working on the local minikube cluster

--- gce/serviceaccount.sh
+++ gce/serviceaccount.sh
@@ -15,11 +15,49 @@ bash ./serviceaccount/create.sh wikibase-dev-api default
 ## add a secret to the cluster
 # kubectl apply -f .serviceaccount/output/secret-wikibase-dev-api.yaml
 
+## sql
+kubectl apply -f definitions/secret-creators/role.yaml
+kubectl apply -f tasks/init-sql-secrets.yaml
+
+cd sql/
+helmfile --environment local apply
+
+kubectl apply -f tasks/init-sql.yaml 
+
+## create service account and get secretname by 
+## kubectl describe service
+
 ## localhost alternative
 #
-# kubectl apply -f - <<EOF
-# apiVersion: v1
-# kind: ServiceAccount
-# metadata:
-#   name: wikibase-dev-api
-# EOF
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: wikibase-dev-api
+EOF
+
+cd ../api
+helmfile --environment local apply
+
+## Apply queryserivice
+
+cd ../queryservice
+helmfile --environment local apply
+
+cd ../queryservice-gateway
+helmfile --environment local apply
+
+cd ../queryservice-ui
+helmfile --environment local apply
+
+cd ../queryservice-updater
+helmfile --environment local apply
+
+cd ../mediawiki135
+helmfile --environment local apply
+
+cd ../platform-nginx
+helmfile --environment local apply
+
+
+
