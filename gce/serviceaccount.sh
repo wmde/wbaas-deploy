bcc1e504427a503d3945c94a3853e0872075c625
addshore <addshorewiki@gmail.com>
2021-10-11 14:14:17 +0100

    Cleanup local dev scratch script

--- gce/serviceaccount.sh
+++ gce/serviceaccount.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This file origionally containers the code to create and get keys from service accounts.
+# Service account creation is now done in Terraform
+# Key getting is still done in here right now
+
 ## Listing existing service accounts
 # gcloud iam service-accounts list
 
@@ -11,53 +15,3 @@ bash ./serviceaccount/create.sh certman-dns01-solver cert-manager
 
 ## Create API service account for api (stackdriver, logos)
 bash ./serviceaccount/create.sh wikibase-dev-api default
-
-## add a secret to the cluster
-# kubectl apply -f .serviceaccount/output/secret-wikibase-dev-api.yaml
-
-## sql
-kubectl apply -f definitions/secret-creators/role.yaml
-kubectl apply -f tasks/init-sql-secrets.yaml
-
-cd sql/
-helmfile --environment local apply
-
-kubectl apply -f tasks/init-sql.yaml 
-
-## create service account and get secretname by 
-## kubectl describe service
-
-## localhost alternative
-#
-kubectl apply -f - <<EOF
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: wikibase-dev-api
-EOF
-
-cd ../api
-helmfile --environment local apply
-
-## Apply queryserivice
-
-cd ../queryservice
-helmfile --environment local apply
-
-cd ../queryservice-gateway
-helmfile --environment local apply
-
-cd ../queryservice-ui
-helmfile --environment local apply
-
-cd ../queryservice-updater
-helmfile --environment local apply
-
-cd ../mediawiki135
-helmfile --environment local apply
-
-cd ../platform-nginx
-helmfile --environment local apply
-
-
-
