0d5e8aa5196ff935923c44fd1aca48271949729d
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2021-10-12 16:34:25 +0100

    Mark domain_mailgun_key as not required

--- tf/env/prod/variables.tf
+++ tf/env/prod/variables.tf
@@ -32,4 +32,6 @@ variable "domain_mailgun_key" {
   type = string
   description = "User provided domain key for API"
   sensitive = true
+  default = "" # apparently this doesn't mean default is empty string but rather default is not defined. This means it won't prompt the user
+  # but also won't override the value that is in the state
 }
