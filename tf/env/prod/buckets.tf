450aaf5157a0daf0efdb37ebe2f0a3dbc43463b5
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2021-10-28 10:13:53 +0100

    Add write access to bucket

--- tf/env/prod/buckets.tf
+++ tf/env/prod/buckets.tf
@@ -1,5 +1,6 @@
 module "staging-buckets" {
   source = "./../../modules/buckets"
   project_prefix = "wikibase-dev"
+  static_bucket_writer_account = google_service_account.dev-api.email
 }
 
--- tf/modules/buckets/main.tf
+++ tf/modules/buckets/main.tf
@@ -2,10 +2,17 @@ resource "google_storage_bucket" "static" {
   name          = "${var.project_prefix}-static"
   location      = "EU"
   force_destroy = false
+
 }
 
 resource "google_storage_bucket_access_control" "static-public" {
   bucket = google_storage_bucket.static.name
   role   = "READER"
   entity = "allUsers"
+}
+
+resource "google_storage_bucket_access_control" "static-writer" {
+  bucket = google_storage_bucket.static.name
+  role   = "WRITER"
+  entity = "user-${var.static_bucket_writer_account}"
 }
