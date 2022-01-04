70907e6b6271c82a5a534e2f3ac3e118351659e5
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-01-04 10:09:24 +0000

    Revert "TF: bump node count for perf testing"
    This reverts commit 5b148a70bd8c06b326cc5998d79e2b20b81514be.

--- tf/env/prod/cluster.tf
+++ tf/env/prod/cluster.tf
@@ -44,7 +44,7 @@ resource "google_container_node_pool" "medium" {
 resource "google_container_node_pool" "standard" {
     cluster = "wbaas-2"
     name                = "standard-pool"
-    node_count          = 3
+    node_count          = 2
     node_locations      = [
         "europe-west3-a",
     ]
