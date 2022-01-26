0f8b93f34f30f48dbc23796cfac24f038a5287e2
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-01-26 17:29:49 +0000

    Remove old staging bucket

--- tf/env/staging/terraform.tf
+++ tf/env/staging/terraform.tf
@@ -22,22 +22,3 @@ resource "google_storage_bucket_iam_member" "tf-state-staging-iam-member" {
   role = "roles/storage.objectAdmin"
   member = "user:${each.value}"
 }
-
-# A non public bucket for storing shared terraform state (from remote-state setup)
-resource "google_storage_bucket" "tf-state" {
-  name          = "wikibase-cloud-tf-state-prod"
-  location      = "EU"
-  force_destroy = false
-  uniform_bucket_level_access = true
-  versioning {
-      enabled = true
-  }
-}
-
-# Give everyone access to the state bucket
-resource "google_storage_bucket_iam_member" "tf-state-iam-member" {
-  for_each = var.terraformers
-  bucket = google_storage_bucket.tf-state.name
-  role = "roles/storage.objectAdmin"
-  member = "user:${each.value}"
-}
