0deb3e1ac1eb9d4170b01a91551690ae74504635
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-01-26 17:14:41 +0000

    TF: add new staging state bucket

--- tf/env/staging/remote-state/main.tf
+++ tf/env/staging/remote-state/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 # A non public bucket for storing shared terraform state
 resource "google_storage_bucket" "tf-state" {
-  name          = "wikibase-cloud-tf-state-prod"
+  name          = "wikibase-cloud-tf-state-staging"
   location      = "EU"
   force_destroy = false
   uniform_bucket_level_access = true
--- tf/env/staging/terraform.tf
+++ tf/env/staging/terraform.tf
@@ -5,6 +5,24 @@ terraform {
   }
 }
 
+resource "google_storage_bucket" "tf-state-staging" {
+  name          = "wikibase-cloud-tf-state-staging"
+  location      = "EU"
+  force_destroy = false
+  uniform_bucket_level_access = true
+  versioning {
+      enabled = true
+  }
+}
+
+# Give everyone access to the state bucket
+resource "google_storage_bucket_iam_member" "tf-state-staging-iam-member" {
+  for_each = var.terraformers
+  bucket = google_storage_bucket.tf-state-staging.name
+  role = "roles/storage.objectAdmin"
+  member = "user:${each.value}"
+}
+
 # A non public bucket for storing shared terraform state (from remote-state setup)
 resource "google_storage_bucket" "tf-state" {
   name          = "wikibase-cloud-tf-state-prod"
