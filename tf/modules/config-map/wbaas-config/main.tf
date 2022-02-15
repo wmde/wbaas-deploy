190b079d8aa5ffe0535be85de880e3a44cd7e134
toan <tobias.andersson@wikimedia.de>
2022-02-15 19:17:13 +0100

    Split up into files

--- tf/modules/config-map/wbaas-config/main.tf
+++ tf/modules/config-map/wbaas-config/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
-}
-
-variable "cname_record" {
-  type = string
-  description = "CNAME_RECORD to be used for custom domains"
-}
-
 resource "kubernetes_config_map" "wbaas" {
   metadata {
     name = "wbaas-config"
