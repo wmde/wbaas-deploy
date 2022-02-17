8fc1077ff3be45f00642ed731fd6c6f39ea218a2
Tobias <tobias.andersson@wikimedia.de>
2022-02-17 15:50:21 +0100

    [T301140.2] Add real cname record to staging (#165)
    * Add real cname record to staging

--- tf/env/staging/dns.tf
+++ tf/env/staging/dns.tf
@@ -67,7 +67,6 @@ resource "google_dns_record_set" "dev-wildcard-A" {
     type         = "A"
 }
 
-
 resource "google_dns_record_set" "dev-MailGun-record" {
     for_each = {
         for index, record in mailgun_domain.default.sending_records:
@@ -77,4 +76,14 @@ resource "google_dns_record_set" "dev-MailGun-record" {
     managed_zone = google_dns_managed_zone.dev.name
     type = each.value.record_type
     rrdatas = [ replace("\"${each.value.value}\"", "/^\"eu.mailgun.org\"$/", "eu.mailgun.org." ) ]
+}
+
+resource "google_dns_record_set" "dev-dyna-A" {
+    managed_zone = google_dns_managed_zone.dev.name
+    name         = module.wbaas-config.cname_record
+    rrdatas      = [
+        google_compute_address.default.address,
+    ]
+    ttl          = 300
+    type         = "A"
 }
