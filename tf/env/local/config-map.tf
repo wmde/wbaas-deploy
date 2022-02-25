8a955d8e52c9cdd165b722525836fc7d1c9bc75b
toan <tobias.andersson@wikimedia.de>
2022-02-25 13:13:56 +0100

    It was deemed better to hardcode these values
    Reverting the config map for localhost

--- tf/env/local/config-map.tf
+++ tf/env/local/config-map.tf
@@ -3,10 +3,3 @@ module "wbaas-config" {
   
   cname_record = "sites-1.dyna.wbaas.localhost."
 }
-
-module "wbaas-mediawiki-config" {
-  source = "./../../modules/config-map/wbaas-mediawiki-config"
-
-  wikibase_concept_uri_scheme = "http://"
-}
-
--- tf/modules/config-map/wbaas-mediawiki-config/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "wikibase_concept_uri_scheme" {
-  type = string
-  description = "Concept URI scheme for Wikibase"
-}
