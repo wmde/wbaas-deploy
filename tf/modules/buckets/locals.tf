dfe250b108fda728eb18b0b5c5e95f843e25a5b3
toan <tobias.andersson@wikimedia.de>
2022-03-03 17:33:14 +0100

    Add first version of logo-bucket backup terraform resources
    Will be wrapped up soon :)

--- tf/modules/buckets/locals.tf
+++ tf/modules/buckets/locals.tf
@@ -1,3 +1,5 @@
 locals {
     gcs_api_static_bucket_name = "${var.project_prefix}-static"
+    gcs_api_static_bucket_backup_name = "${var.project_prefix}-static-backup"
+    transfer_service_id = "project-658442145969@storage-transfer-service.iam.gserviceaccount.com"
 }
