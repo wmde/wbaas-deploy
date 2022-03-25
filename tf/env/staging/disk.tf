d5f001a494b62abd7689a03202e123c4ccfd062e
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-03-25 18:38:46 +0100

    Add snapshot policy and add it to sql-secondary

--- tf/env/staging/disk.tf
+++ tf/env/staging/disk.tf
@@ -19,3 +19,55 @@ resource "google_compute_disk" "data-sql-mariadb-secondary" {
         "goog-gke-volume" = ""
     }
 }
+
+resource "kubernetes_persistent_volume" "data-sql-mariadb-secondary" {
+
+    provider = kubernetes.wbaas-2
+  metadata {
+    name = "pv-data-sql-mariadb-secondary-0"
+  }
+  spec {
+    capacity = {
+      storage = "60Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+
+    claim_ref {
+        name = "data-sql-mariadb-secondary-0"
+        namespace = "default"
+    }
+    persistent_volume_source {
+        gce_persistent_disk {
+          pd_name = google_compute_disk.data-sql-mariadb-secondary.name
+        }
+    }
+  }
+}
+
+resource "google_compute_resource_policy" "wbcloud-nightly" {
+  name   = "wbcloud-nightly-west-to-north-7d-1"
+  description = "A nightly snapshot that is retained for 7 days from europe-west3 to the europe-north1. Snapshots kept on disk deletion."
+  region = "europe-west3"
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle  = 1
+        start_time     = "03:00"
+      }
+    }
+    
+    retention_policy {
+      max_retention_days    = 7
+      on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+    }
+
+    snapshot_properties {
+      storage_locations = ["europe-north1"]
+    }
+  }
+}
+
+resource "google_compute_region_disk_resource_policy_attachment" "attachment" {
+  name = google_compute_resource_policy.wbcloud-nightly.name
+  disk = google_compute_disk.data-sql-mariadb-secondary.name
+}
