32889c49cb01e33f3a58d249b38aaf36b2a91bb7
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-03-28 11:58:29 +0200

    Add lifecycle ignore for labels and description

--- tf/env/staging/disk.tf
+++ tf/env/staging/disk.tf
@@ -5,19 +5,9 @@ resource "google_compute_disk" "data-sql-mariadb-secondary" {
   size  = 60 # GB
   physical_block_size_bytes = 4096
   project = local.project_id
-
-  description = jsonencode(
-            {
-              "gkebackup.gke.io/created-for/volume-restore" = "vr-48b73253ea7f65d"
-              "kubernetes.io/created-for/pv/name"           = "pvc-429ef8a6-8500-4c71-86cc-f027888d9f4a"
-              "kubernetes.io/created-for/pvc/name"          = "data-sql-mariadb-primary-0"
-              "kubernetes.io/created-for/pvc/namespace"     = "default"
-            }
-    )
-
-    labels = {
-        "goog-gke-volume" = ""
-    }
+  lifecycle {
+    ignore_changes = [ labels, description ]
+  }
 }
 
 resource "kubernetes_persistent_volume" "data-sql-mariadb-secondary" {
