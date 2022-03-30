dcba57d1b1ebf19d2bd9537ee9047b924ee90d2b
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-03-30 15:32:03 +0200

    Remove disks module from production

--- tf/env/production/disks.tf
+++
@@ -1,9 +0,0 @@
-module "wbaas3-disks" {
-  source = "./../../modules/disks"
-
-  providers = {
-    kubernetes = kubernetes.wbaas-3
-  }
-
-  sql-replica-disk-name = "pvc-8c1ba5fc-4447-46ef-a279-656e30636897"
-}
