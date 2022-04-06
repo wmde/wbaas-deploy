d365630c67768ca44da349c5f6c1181dec814398
toan <tobias.andersson@wikimedia.de>
2022-04-06 17:40:51 +0200

    This adds the production deployment off wbaas-backup
    - adds the bucket configuration
    - installs the chart
    - adds the backup key secret

--- tf/env/production/buckets.tf
+++ tf/env/production/buckets.tf
@@ -1,10 +1,11 @@
 module "production-buckets" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/buckets/?ref=9ff399e53b55f6be1939c458ea2003956b7baa72"
+  #source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/buckets/?ref=<COMMIT_HASH>"
+  source = "./../../modules/buckets"
   providers = {
     kubernetes = kubernetes.wbaas-3
   }
   project_prefix = "wikibase-cloud"
   static_bucket_writer_account = google_service_account.api.email
-  backup_bucket_object_admins = var.terraformers
+  user_object_admins = var.terraformers
 }
 
--- tf/env/production/kubernetes-secrets.tf
+++ tf/env/production/kubernetes-secrets.tf
@@ -1,5 +1,6 @@
 module "wbaas-k8s-secrets" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/k8s-secrets?ref=9ff399e53b55f6be1939c458ea2003956b7baa72"
+  #source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/k8s-secrets?ref=<COMMIT_HASH>"
+  source = "./../../modules/k8s-secrets"
   providers = {
     kubernetes = kubernetes.wbaas-3
   }
@@ -12,6 +13,7 @@ module "wbaas-k8s-secrets" {
   sql_password_replication = random_password.sql-passwords["production-replication"].result
   sql_password_api = random_password.sql-passwords["production-api"].result
   sql_password_mediawiki_db_manager = random_password.sql-passwords["production-mediawiki-db-manager"].result
+  sql_password_backup_manager = random_password.sql-passwords["production-backup-manager"].result
   redis_password = random_password.redis-password.result
   recaptcha_v3_site_key = var.recaptcha_v3_site_key
   recaptcha_v3_secret = var.recaptcha_v3_secret
@@ -21,5 +23,6 @@ module "wbaas-k8s-secrets" {
   api_passport_private_key = tls_private_key.api-passport.private_key_pem
   api_app_key = random_password.api-app-key.result
   api_app_jwt_secret = random_password.api-app-jwt-secret.result
+  logical_backup_openssl_secret = random_password.logical_backup_random_password.result
 }
 
