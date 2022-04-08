fef908d315fd1ba87d9a8d2c7e1cbe2703db8ec3
toan <tobias.andersson@wikimedia.de>
2022-04-08 14:18:20 +0200

    Revert retention policy on sql backup bucket
    This seems to also prevent files being written. :facepalm:
    
    https://github.com/GoogleCloudPlatform/gcsfuse/issues/355

--- tf/modules/buckets/main.tf
+++ tf/modules/buckets/main.tf
@@ -11,11 +11,6 @@ resource "google_storage_bucket" "sql-backup" {
   location      = "EUROPE-WEST6"
   force_destroy = false
 
-  retention_policy {
-    is_locked = false
-    retention_period = 604700
-  }
-
   // only keep versions for N days
   lifecycle_rule {
     action {
