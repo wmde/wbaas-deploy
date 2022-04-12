26e9a353f5832e389a96507c658ccdf5baf39ff4
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-04-12 15:15:33 +0200

    Remove module from prod in order to use versioned module after merging

--- tf/env/production/monitoring.tf
+++
@@ -1,8 +0,0 @@
-module "production-monitoring" {
-  source = "./../../modules/monitoring"
-  providers = {
-    kubernetes = kubernetes.wbaas-3
-  }
-  cluster_name = local.production_cluster_name
-  email_group = "wb-cloud-monitoring@wikimedia.de"
-}
