fc6f7301154fc6a9f352bd325339c3d0dbac7c9e
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-04-13 13:29:16 +0200

    Add versioned monitoring tf module to production

---
+++ tf/env/production/monitoring.tf
@@ -0,0 +1,10 @@
+module "production-monitoring" {
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-0"
+  
+  providers = {
+    kubernetes = kubernetes.wbaas-3
+  }
+
+  cluster_name = local.production_cluster_name
+  email_group = "wb-cloud-monitoring@wikimedia.de"
+}
