609753c90b9d8318aaf94a013ff89bada14b81b8
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-04-29 10:11:27 +0200

    prod: use tf-module-monitoring-3 again

--- tf/env/production/monitoring.tf
+++ tf/env/production/monitoring.tf
@@ -1,5 +1,5 @@
 module "production-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-4"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-3"
   
   providers = {
     kubernetes = kubernetes.wbaas-3
