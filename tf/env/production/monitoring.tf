5478a20e59bbf1379c48252f60518a9a254a59e6
Thomas Arrow <tarrow@users.noreply.github.com>
2022-05-24 11:38:35 +0100

    tf: prod: Add metric for monitoring requests to site (#332)

--- tf/env/production/monitoring.tf
+++ tf/env/production/monitoring.tf
@@ -8,3 +8,40 @@ module "production-monitoring" {
   cluster_name = local.production_cluster_name
   email_group = "wb-cloud-monitoring@wikimedia.de"
 }
+
+resource "google_logging_metric" "production-site-request-count" {
+    description      = "Count of requests to sites"
+    filter           = <<-EOT
+        resource.labels.container_name="nginx-ingress-controller"
+        resource.type="k8s_container"
+        -textPayload:"GoogleStackdriverMonitoring"
+        -textPayload:"cert-manager"
+        -textPayload:"Widar"
+    EOT
+    label_extractors = {
+        "domain"     = "REGEXP_EXTRACT(textPayload, \"\\\\w+ https:\\\\/\\\\/([^\\\\/]+)\")"
+        "httpMethod" = "REGEXP_EXTRACT(textPayload, \"(\\\\w+) https:\\\\/\\\\/[^ ]+ [^ ]+ \\\\d+\")"
+        "statuscode" = "REGEXP_EXTRACT(textPayload, \"\\\\w+ https:\\\\/\\\\/[^ ]+ [^ ]+ (\\\\d+)\")"
+    }
+    name             = "production-site-request-count"
+    project          = "wikibase-cloud"
+
+    metric_descriptor {
+        metric_kind = "DELTA"
+        unit        = "1"
+        value_type  = "INT64"
+
+        labels {
+            key        = "domain"
+            value_type = "STRING"
+        }
+        labels {
+            key        = "httpMethod"
+            value_type = "STRING"
+        }
+        labels {
+            key        = "statuscode"
+            value_type = "STRING"
+        }
+    }
+}
