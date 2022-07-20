c19ab5df476e6c9790f791350cb388206480a93f
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2022-07-20 18:00:00 +0200

    tf-module-monitoring-10: Add policies for SQL, ES & QS PVs (#455)
    Co-authored-by: rosalie <perside.rosalie@wikimedia.de>

---
+++ tf/modules/monitoring/elasticsearch-pv.tf
@@ -0,0 +1,42 @@
+locals {
+  elasticsearch_pv_critical_utilization_threshold = 0.85 # 85%, alert triggers if metric is above this value
+}
+
+resource "google_monitoring_alert_policy" "alert_policy_elasticsearch_pv_critical_utilization" {
+  display_name = "(${var.cluster_name}): critical Elastic Search PV utilization"
+  combiner     = "OR"
+  notification_channels = [
+    google_monitoring_notification_channel.monitoring_email_group.name
+  ]
+
+  documentation {
+    content = "Alert triggers when the disk space utilization of the Elastic Search data PV is reaching a critical level (over ${local.elasticsearch_pv_critical_utilization_threshold * 100}%)"
+  }
+
+  conditions {
+    display_name = "(${var.cluster_name}): critical Elastic Search PV utilization"
+    condition_threshold {
+      filter = <<-EOT
+                metric.type="kubernetes.io/pod/volume/utilization"
+                resource.label."cluster_name"=${var.cluster_name}
+                resource.label."pod_name"=starts_with("elasticsearch-")
+                resource.type="k8s_pod"
+                metric.label."volume_name"="elasticsearch-master"
+            EOT
+
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = local.elasticsearch_pv_critical_utilization_threshold
+
+      aggregations {
+        alignment_period     = "120s"
+        per_series_aligner   = "ALIGN_MEAN"
+        cross_series_reducer = "REDUCE_SUM"
+      }
+
+      trigger {
+        count = 1
+      }
+    }
+  }
+}
---
+++ tf/modules/monitoring/sql-pv.tf
@@ -0,0 +1,81 @@
+locals {
+  sql_pv_critical_utilization_threshold = 0.85 # 85%, alert triggers if metric is above this value
+}
+
+resource "google_monitoring_alert_policy" "alert_policy_sql_primary_pv_critical_utilization" {
+  display_name = "(${var.cluster_name}): critical SQL primary PV utilization"
+  combiner     = "OR"
+  notification_channels = [
+    google_monitoring_notification_channel.monitoring_email_group.name
+  ]
+
+  documentation {
+    content = "Alert triggers when the disk space utilization of the SQL primary data PV is reaching a critical level (over ${local.sql_pv_critical_utilization_threshold * 100}%)"
+  }
+
+  conditions {
+    display_name = "(${var.cluster_name}): critical SQL primary PV utilization"
+    condition_threshold {
+      filter = <<-EOT
+                metric.type="kubernetes.io/pod/volume/utilization"
+                resource.label."cluster_name"=${var.cluster_name}
+                resource.label."pod_name"="sql-mariadb-primary-0"
+                resource.type="k8s_pod"
+                metric.label."volume_name"="data"
+            EOT
+
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = local.sql_pv_critical_utilization_threshold
+
+      aggregations {
+        alignment_period     = "120s"
+        per_series_aligner   = "ALIGN_MEAN"
+        cross_series_reducer = "REDUCE_SUM"
+      }
+
+      trigger {
+        count = 1
+      }
+    }
+  }
+}
+
+resource "google_monitoring_alert_policy" "alert_policy_sql_secondary_pv_critical_utilization" {
+  display_name = "(${var.cluster_name}): critical SQL secondary PV utilization"
+  combiner     = "OR"
+  notification_channels = [
+    google_monitoring_notification_channel.monitoring_email_group.name
+  ]
+
+  documentation {
+    content = "Alert triggers when the disk space utilization of the SQL secondary data PV is reaching a critical level (over ${local.sql_pv_critical_utilization_threshold * 100}%)"
+  }
+
+  conditions {
+    display_name = "(${var.cluster_name}): critical SQL secondary PV utilization"
+    condition_threshold {
+      filter = <<-EOT
+                metric.type="kubernetes.io/pod/volume/utilization"
+                resource.label."cluster_name"=${var.cluster_name}
+                resource.label."pod_name"="sql-mariadb-secondary-0"
+                resource.type="k8s_pod"
+                metric.label."volume_name"="data"
+            EOT
+
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = local.sql_pv_critical_utilization_threshold
+
+      aggregations {
+        alignment_period     = "120s"
+        per_series_aligner   = "ALIGN_MEAN"
+        cross_series_reducer = "REDUCE_SUM"
+      }
+
+      trigger {
+        count = 1
+      }
+    }
+  }
+}
