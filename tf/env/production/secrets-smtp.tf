f41d16ff990bfd5ba2ce3f51d06f73b0cf4ebde8
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2022-09-23 15:58:00 +0200

    Rotate production mailgun SMTP password (#540)

--- tf/env/production/secrets-smtp.tf
+++ tf/env/production/secrets-smtp.tf
@@ -1,6 +1,9 @@
-# Use for sql dbs
 resource "random_password" "smtp-password" {
   length           = 32
   special          = true
   override_special = "_%@"
+
+  keepers = {
+    value = 1
+  }
 }
