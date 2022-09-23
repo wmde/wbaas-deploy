9cdd95c36e7067c29265b63ade90d4e4bed10241
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-09-23 16:11:17 +0200

    Release tf-module-k8s-secrets-1

--- tf/modules/k8s-secrets/variables.tf
+++ tf/modules/k8s-secrets/variables.tf
@@ -1,11 +1,3 @@
-variable "domain_mailgun_key" {
-  type = string
-  description = "User provided domain key for API"
-  sensitive = true
-  default = "" # apparently this doesn't mean default is empty string but rather default is not defined. This means it won't prompt the user
-  # but also won't override the value that is in the state
-}
-
 variable "smtp_username" {
   type = string
   description = "Username for SMTP server"
