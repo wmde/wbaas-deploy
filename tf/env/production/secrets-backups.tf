4728a7ff321fcfde6780f0856f95c42ec70f1df6
Perside Rosalie <perside.rosalie@wikimedia.de>
2022-10-19 14:45:13 +0200

    Prod: TF rotate backup_random_password (#554)

--- tf/env/production/secrets-backups.tf
+++ tf/env/production/secrets-backups.tf
@@ -2,4 +2,7 @@ resource "random_password" "logical_backup_random_password" {
   length           = 32
   special          = true
   override_special = "_%@"
+  keepers = {
+        rotate = 1
+  }
 }
