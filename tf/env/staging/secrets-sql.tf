d6c802c52878b354ce33bbcdf14ae2960bd18bb1
Thomas Arrow <tarrow@users.noreply.github.com>
2022-10-26 10:52:37 +0100

    Revert "Staging: Rotate root sql password (#563)" (#564)
    This reverts commit 7210d677ec3241ac39116a9c0d9dfbe8e1e1b9bc.

--- tf/env/staging/secrets-sql.tf
+++ tf/env/staging/secrets-sql.tf
@@ -5,20 +5,3 @@ resource "random_password" "sql-passwords" {
   special          = true
   override_special = "_%@"
 }
-
-resource "random_password" "sql-root-password" {
-  length           = 32
-  special          = true
-  override_special = "_%@"
-}
-
-resource "kubernetes_secret" "sql-root-old-secret-password" {
-  metadata {
-    name = "sql-root-old-secret-password"
-    namespace = "default"
-  }
-
-  binary_data = {
-    "mariadb-root-password" = base64encode(random_password.sql-passwords["staging-root"].result)
-  }
-}
