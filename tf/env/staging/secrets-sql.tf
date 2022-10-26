e4c192053d002330eab660be8a77b8ffad156eae
Thomas Arrow <tarrow@users.noreply.github.com>
2022-10-26 11:03:28 +0100

    Staging: Rotate root sql password (#565)
    This temporarally adds a new k8s secret with the old root password
    in so we can login to change it to the new one.
    After applying this patch you must run the resetRootSqlSecretJob.
    
    This is a reattempt of #563 with the kubernetes provider correctly
    set.
    
    Bug: T321051
    
    Co-authored-by: Perside Rosalie <perside.rosalie@wikimedia.de>

--- tf/env/staging/secrets-sql.tf
+++ tf/env/staging/secrets-sql.tf
@@ -5,3 +5,21 @@ resource "random_password" "sql-passwords" {
   special          = true
   override_special = "_%@"
 }
+
+resource "random_password" "sql-root-password" {
+  length           = 32
+  special          = true
+  override_special = "_%@"
+}
+
+resource "kubernetes_secret" "sql-root-old-secret-password" {
+  provider = kubernetes.wbaas-2
+  metadata {
+    name = "sql-root-old-secret-password"
+    namespace = "default"
+  }
+
+  binary_data = {
+    "mariadb-root-password" = base64encode(random_password.sql-passwords["staging-root"].result)
+  }
+}
