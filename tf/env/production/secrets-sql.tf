a4977966b45eb36e6e978c113cc2a83c5646d38f
Perside Rosalie <perside.rosalie@wikimedia.de>
2022-11-01 12:43:22 +0100

    Prod: Rotate root sql password (#568)
    This temporarally adds a new k8s secret with the old root password
    in so we can login to change it to the new one.
    After applying this patch you must run the resetRootSqlSecretJob.
    
    Bug: T321051

--- tf/env/production/secrets-sql.tf
+++ tf/env/production/secrets-sql.tf
@@ -5,3 +5,22 @@ resource "random_password" "sql-passwords" {
   special          = true
   override_special = "_%@"
 }
+
+
+resource "random_password" "sql-root-password" {
+  length           = 32
+  special          = true
+  override_special = "_%@"
+}
+
+resource "kubernetes_secret" "sql-root-password-old" {
+  provider = kubernetes.wbaas-3
+  metadata {
+    name = "sql-root-password-old"
+    namespace = "default"
+  }
+
+  binary_data = {
+    "mariadb-root-password" = base64encode(random_password.sql-passwords["staging-root"].result)
+  }
+}
