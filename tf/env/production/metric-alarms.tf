e30c33d75456eefeefce85db297978ff6382d709
Frederik Ring <frederik.ring@wikimedia.de>
2023-04-24 16:18:00 +0200

    fix(tf): remove duplicate alarm definitions (#879)

--- tf/env/production/metric-alarms.tf
+++
@@ -1,6 +0,0 @@
-module "metric-alarms" {
-  source                      = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/metric-alarms?ref=tf-module-metric-alarms-1"
-  cluster_name                = "wbaas-3"
-  environment                 = "production"
-  monitoring_email_group_name = google_monitoring_notification_channel.monitoring_email_group.name
-}
