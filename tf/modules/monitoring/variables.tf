a3e99c28bfabc4506383a4120b321c8a0ad0e6d7
Frederik Ring <frederik.ring@wikimedia.de>
2023-04-24 15:47:30 +0200

    chore(tf): merge monitoring and metric-alarms modules

--- tf/modules/monitoring/variables.tf
+++ tf/modules/monitoring/variables.tf
@@ -1,36 +1,41 @@
 variable "cluster_name" {
-  type = string
+  type        = string
   description = "Name of the cluster"
 }
 
 variable "monitoring_email_group_name" {
-  type = string
+  type        = string
   description = "Name of the monitoring resource that will receive alerts"
 }
 
 variable "elasticsearch_metrics" {
-  type    = set(string)
+  type        = set(string)
   description = "Metrics to look for in the json payload"
   default = [
-      "heap.percent",
-      "ram.percent",
-      "disk.used_percent",
-      "load_1m"
-      ]
+    "heap.percent",
+    "ram.percent",
+    "disk.used_percent",
+    "load_1m"
+  ]
 }
 
 variable "platform_summary_metrics" {
-  type    = set(string)
+  type        = set(string)
   description = "Metrics to look for in the json payload"
   default = [
-      "active",
-      "total",
-      "deleted",
-      "inactive",
-      "empty",
-      "total_non_deleted_users",
-      "total_non_deleted_active_users",
-      "total_non_deleted_pages",
-      "total_non_deleted_edits",
-      ]
+    "active",
+    "total",
+    "deleted",
+    "inactive",
+    "empty",
+    "total_non_deleted_users",
+    "total_non_deleted_active_users",
+    "total_non_deleted_pages",
+    "total_non_deleted_edits",
+  ]
+}
+
+variable "environment" {
+  type        = string
+  description = "Human friendly name of the target environment. Example: production"
 }
