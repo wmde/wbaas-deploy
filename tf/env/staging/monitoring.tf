0b9cef434d719a8163d0d82df762ed0ad76a5f63
Frederik Ring <frederik.ring@wikimedia.de>
2023-04-24 16:08:12 +0200

    feat(monitoring): add alerting policy for es cluster health (#877)

--- tf/env/staging/monitoring.tf
+++ tf/env/staging/monitoring.tf
@@ -1,8 +1,9 @@
 module "staging-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-15"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-16"
   providers = {
     kubernetes = kubernetes.wbaas-2
   }
-  cluster_name = local.staging_cluster_name
+  cluster_name                = local.staging_cluster_name
+  environment                 = "staging"
   monitoring_email_group_name = google_monitoring_notification_channel.monitoring_email_group.name
 }
