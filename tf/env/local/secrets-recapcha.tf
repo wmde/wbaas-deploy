c8416a63bf9037855c8dbc51af5514975ef81e25
Frederik Ring <frederik.ring@wikimedia.de>
2023-06-01 14:31:26 +0200

    Revert "feat(k8s): add namespace for api jobs and add secrets to both namespaces (#885)" (#930)
    This reverts commit 795e9f751b48798879edb1e395fc09cf22dd0883.

--- tf/env/local/secrets-recapcha.tf
+++ tf/env/local/secrets-recapcha.tf
@@ -1,9 +1,8 @@
 resource "kubernetes_secret" "recaptcha-v3-dev-secrets" {
-  for_each = toset(["default", "api-jobs"])
   metadata {
     name = "recaptcha-v3-dev-secrets"
     # default as staging
-    namespace = each.value
+    namespace = "default"
   }
 
   data = {
@@ -12,26 +11,15 @@ resource "kubernetes_secret" "recaptcha-v3-dev-secrets" {
   }
 }
 
-moved {
-  from = kubernetes_secret.recaptcha-v3-dev-secrets
-  to   = kubernetes_secret.recaptcha-v3-dev-secrets["default"]
-}
-
 resource "kubernetes_secret" "recaptcha-v2-dev-secrets" {
-  for_each = toset(["default", "api-jobs"])
   metadata {
     name = "recaptcha-v2-dev-secrets"
     # default as staging
-    namespace = each.value
+    namespace = "default"
   }
 
   data = {
     "site_key" = var.recaptcha_v2_dev_site_key,
     "secret_key" = var.recaptcha_v2_dev_secret
   }
-}
-
-moved {
-  from = kubernetes_secret.recaptcha-v2-dev-secrets
-  to   = kubernetes_secret.recaptcha-v2-dev-secrets["default"]
 }
