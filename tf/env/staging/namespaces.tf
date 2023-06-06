fe74678964851b91b438838d0381ba9467931a46
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-06-06 10:22:33 +0200

    Add wbaas-2 providers to tf k8s namespace conf (#935)

--- tf/env/staging/namespaces.tf
+++ tf/env/staging/namespaces.tf
@@ -1,10 +1,14 @@
 resource "kubernetes_namespace" "api-job-namespace" {
+  provider = kubernetes.wbaas-2
+
   metadata {
     name = "api-jobs"
   }
 }
 
 resource "kubernetes_resource_quota" "api-jobs-podquota" {
+  provider = kubernetes.wbaas-2
+  
   metadata {
     name      = "api-jobs-podquota"
     namespace = kubernetes_namespace.api-job-namespace.metadata[0].name
