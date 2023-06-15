a36e26355b05ecab66fbcc070535e3b9cdbdc289
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-06-15 19:14:38 +0200

    Removing faulty second maintenace p[lociy block (#957)

--- tf/env/staging/cluster.tf
+++ tf/env/staging/cluster.tf
@@ -55,14 +55,4 @@ resource "google_container_node_pool" "wbaas-2_large" {
         max_surge       = 1
         max_unavailable = 0
     }
-
-    maintenance_policy {
-        recurring_window {
-            # timezone: UTC
-            # "Every monday between 04.00 and 16.00 Berlin time"
-            start_time = "1970-01-01T02:00:00Z"
-            end_time   = "1970-01-01T14:00:00Z"
-            recurrence = "FREQ=WEEKLY;BYDAY=MO"
-        }
-    }
 }
