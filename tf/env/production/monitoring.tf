a130d98bb50bdfe75b5602025ba5558e7e2ede35
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-06-19 15:30:16 +0200

    Use `tf-module-monitoring-18` (#959)

--- tf/env/production/monitoring.tf
+++ tf/env/production/monitoring.tf
@@ -1,5 +1,5 @@
 module "production-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-17"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-18"
 
   providers = {
     kubernetes = kubernetes.wbaas-3
