7c2995d014564c53718402fa42ff631b4638c45a
Frederik Ring <frederik.ring@wikimedia.de>
2023-08-14 15:58:44 +0200

    feat(k8s): run adhoc jobs in dedicated namespace (#1065)
    * feat(k8s): run adhoc jobs in dedicated namespace
    
    * feat: add ttl for all jobs

--- tf/env/local/namespaces.tf
+++ tf/env/local/namespaces.tf
@@ -16,3 +16,23 @@ resource "kubernetes_resource_quota" "api-jobs-podquota" {
     scopes = ["BestEffort"]
   }
 }
+
+
+resource "kubernetes_namespace" "adhoc-job-namespace" {
+  metadata {
+    name = "adhoc-jobs"
+  }
+}
+
+resource "kubernetes_resource_quota" "adhoc-jobs-podquota" {
+  metadata {
+    name      = "api-jobs-podquota"
+    namespace = kubernetes_namespace.api-job-namespace.metadata[0].name
+  }
+  spec {
+    hard = {
+      pods = 1
+    }
+    scopes = ["BestEffort"]
+  }
+}
--- tf/env/production/namespaces.tf
+++ tf/env/production/namespaces.tf
@@ -20,3 +20,27 @@ resource "kubernetes_resource_quota" "api-jobs-podquota" {
     scopes = ["BestEffort"]
   }
 }
+
+
+resource "kubernetes_namespace" "adhoc-job-namespace" {
+  provider = kubernetes.wbaas-2
+
+  metadata {
+    name = "adhoc-jobs"
+  }
+}
+
+resource "kubernetes_resource_quota" "adhoc-jobs-podquota" {
+  provider = kubernetes.wbaas-2
+
+  metadata {
+    name      = "api-jobs-podquota"
+    namespace = kubernetes_namespace.api-job-namespace.metadata[0].name
+  }
+  spec {
+    hard = {
+      pods = 8
+    }
+    scopes = ["BestEffort"]
+  }
+}
