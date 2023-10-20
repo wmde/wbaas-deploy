22d03b03627f3f70e9ec873a4cc9d4e24c9cc1ef
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-10-20 10:29:42 +0200

    chore(deploy): production: tf-module-monitoring-20 (#1191)
    * chore(deploy): staging: tf-module-monitoring-20
    
    * chore(deploy): production: tf-module-monitoring-20

--- tf/env/production/monitoring.tf
+++ tf/env/production/monitoring.tf
@@ -1,5 +1,5 @@
 module "production-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-18"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-20"
 
   providers = {
     kubernetes = kubernetes.wbaas-3
