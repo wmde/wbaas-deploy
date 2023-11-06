a863ab70f1ce6c6abb7df343093c0fedbd5beba5
Frederik Ring <frederik.ring@wikimedia.de>
2023-11-06 11:33:40 +0100

    fix(local): change variable declaration for recaptcha secrets (#1238)

--- tf/env/local/variables.tf
+++ tf/env/local/variables.tf
@@ -10,24 +10,24 @@ variable "sql-passwords" {
   ]
 }
 
-variable "recaptcha_v3_dev_site_key" {
+variable "recaptcha_v3_site_key" {
   type        = string
   description = "Site key to access recaptcha v3"
   sensitive   = true
 }
 
-variable "recaptcha_v3_dev_secret" {
+variable "recaptcha_v3_secret" {
   type        = string
   description = "Secret key to access recaptcha v3"
   sensitive   = true
 }
-variable "recaptcha_v2_dev_site_key" {
+variable "recaptcha_v2_site_key" {
   type        = string
   description = "Site key to access recaptcha v2"
   sensitive   = true
 }
 
-variable "recaptcha_v2_dev_secret" {
+variable "recaptcha_v2_secret" {
   type        = string
   description = "Secret key to access recaptcha v2"
   sensitive   = true
