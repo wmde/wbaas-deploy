1968e94dc6bef3ada2487a47bc6833185aeca70f
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-12-14 17:10:39 +0100

    Test SQL alerting policy (#1324)
    * use `tf-module-monitoring-23` on staging and prod
    
    * "disable" the backup cronjob for staging and prod
    
    * Update wbaas-backup.values.yaml.gotmpl

--- tf/env/staging/monitoring.tf
+++ tf/env/staging/monitoring.tf
@@ -1,5 +1,5 @@
 module "staging-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-22"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-23"
   providers = {
     kubernetes = kubernetes.wbaas-2
   }
