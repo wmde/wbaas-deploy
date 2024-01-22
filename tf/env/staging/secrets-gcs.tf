9464a168b817e704d79da211e3440b0c777e510f
Frederik Ring <frederik.ring@wikimedia.de>
2024-01-22 11:37:44 +0100

    fix: update buckets module

--- tf/env/staging/secrets-gcs.tf
+++ tf/env/staging/secrets-gcs.tf
@@ -22,24 +22,3 @@ resource "kubernetes_secret" "gcs-hmac-key" {
     "secret-key" = google_storage_hmac_key.dev-backup-upload-key.secret
   }
 }
-
-resource "google_storage_hmac_key" "public-assets-key" {
-  service_account_email = google_service_account.dev-api.email
-}
-
-resource "google_project_iam_member" "public-assets-upload" {
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.dev-api.email}"
-  project = local.project_id
-}
-
-resource "kubernetes_secret" "public-assets-hmac-key" {
-  provider = kubernetes.wbaas-2
-  metadata {
-    name = "public-assets-hmac-key"
-  }
-  data = {
-    "access-key" = google_storage_hmac_key.public-assets-key.access_id
-    "secret-key" = google_storage_hmac_key.public-assets-key.secret
-  }
-}
