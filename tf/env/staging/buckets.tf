e3a4a0ba5a7890ead30a756b57eb087c9aaec6e6
Frederik Ring <frederik.ring@wikimedia.de>
2024-01-23 16:07:39 +0100

    chore(staging): updated bucket module limits key permissions to bucket

--- tf/env/staging/buckets.tf
+++ tf/env/staging/buckets.tf
@@ -1,5 +1,5 @@
 module "staging-buckets" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/buckets?ref=tf-module-buckets-3"
+  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/buckets?ref=tf-module-buckets-4"
   providers = {
     kubernetes = kubernetes.wbaas-2
   }
