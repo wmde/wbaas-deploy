7c4b5ce5a5fd20c0123b022d64ec9be1fa6b6117
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-02-26 15:22:57 +0100

    chore(elasticsearch): use a dedicated node pool for data nodes (#1441)

--- tf/env/production/cluster.tf
+++ tf/env/production/cluster.tf
@@ -59,3 +59,50 @@ resource "google_container_node_pool" "wbaas-3_highmem-16" {
     max_unavailable = 0
   }
 }
+
+resource "google_container_node_pool" "wbaas-3_search-data-pool" {
+  cluster     = "wbaas-3"
+  name_prefix = "search-data-pool-"
+  node_count  = 1
+  node_locations = [
+    "europe-west3-a",
+  ]
+  node_config {
+    disk_size_gb = 32
+    disk_type    = "pd-ssd"
+    machine_type = "n2-highmem-8"
+    metadata = {
+      "disable-legacy-endpoints" = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+    preemptible     = false
+    service_account = "default"
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = false
+    }
+    logging_variant = "MAX_THROUGHPUT"
+    taint = [
+      {
+        key    = "app.kubernetes.io/component"
+        value  = "data"
+        effect = "NO_SCHEDULE"
+      },
+      {
+        key    = "app.kubernetes.io/name"
+        value  = "elasticsearch"
+        effect = "NO_SCHEDULE"
+      }
+    ]
+  }
+  management {
+    auto_upgrade = false
+  }
+}
