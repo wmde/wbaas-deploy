853208945c370b33fda25d582ef492ceed2cf1be
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-02-29 16:33:19 +0100

    chore(cluster): delay cluster update for production (#1459)

--- tf/env/production/cluster.tf
+++ tf/env/production/cluster.tf
@@ -15,7 +15,7 @@ resource "google_container_cluster" "wbaas-3" {
       # "Every monday between 04.00 and 16.00 Berlin time"
       start_time = "1970-01-01T02:00:00Z"
       end_time   = "1970-01-01T14:00:00Z"
-      recurrence = "FREQ=WEEKLY;BYDAY=MO"
+      recurrence = "FREQ=WEEKLY;BYDAY=WE"
     }
   }
 }
