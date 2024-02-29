a41b1393174a85babf1b427c337c8caf455c3453
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-02-29 09:04:06 +0000

    chore(elasticsearch): add dedicated pool for master nodes on staging

--- tf/env/staging/cluster.tf
+++ tf/env/staging/cluster.tf
@@ -57,6 +57,56 @@ resource "google_container_node_pool" "wbaas-2_large" {
   }
 }
 
+resource "google_container_node_pool" "wbaas-2_search-master-pool" {
+  cluster    = "wbaas-2"
+  name       = "search-master-pool"
+  node_count = 3
+  node_locations = [
+    "europe-west3-a",
+  ]
+  node_config {
+    disk_size_gb = 32
+    disk_type    = "pd-standard"
+    machine_type = "n1-standard-1"
+    metadata = {
+      "disable-legacy-endpoints" = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+    preemptible     = false
+    service_account = "default"
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = false
+    }
+    labels = {
+      "wbaas/pool" = "search-master"
+    }
+    taint = [
+      {
+        key    = "wbaas/pool"
+        value  = "search-master"
+        effect = "NO_SCHEDULE"
+      }
+    ]
+  }
+  upgrade_settings {
+    strategy = "BLUE_GREEN"
+    blue_green_settings {
+      standard_rollout_policy {
+        batch_node_count    = 1
+        batch_soak_duration = "28800s"
+      }
+    }
+  }
+}
+
 resource "google_container_node_pool" "wbaas-2_search-data-pool" {
   cluster    = "wbaas-2"
   name       = "search-data-pool"
