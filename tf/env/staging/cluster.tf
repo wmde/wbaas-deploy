21fed621d16aa3d73c65970437884909adfc3b99
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-03-01 14:17:04 +0100

    chore(cluster): remove previous node pool on staging (#1468)

--- tf/env/staging/cluster.tf
+++ tf/env/staging/cluster.tf
@@ -20,43 +20,6 @@ resource "google_container_cluster" "wbaas-2" {
   }
 }
 
-resource "google_container_node_pool" "wbaas-2_large" {
-  cluster    = "wbaas-2"
-  name       = "large-pool"
-  node_count = 3
-  node_locations = [
-    "europe-west3-a",
-  ]
-  node_config {
-    disk_size_gb = 32
-    disk_type    = "pd-standard"
-    machine_type = "n2-standard-8"
-    metadata = {
-      "disable-legacy-endpoints" = "true"
-    }
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-    preemptible     = false
-    service_account = "default"
-    shielded_instance_config {
-      enable_integrity_monitoring = true
-      enable_secure_boot          = false
-    }
-    logging_variant = "MAX_THROUGHPUT"
-  }
-
-  upgrade_settings {
-    max_surge       = 1
-    max_unavailable = 0
-  }
-}
-
 resource "google_container_node_pool" "wbaas-2_compute-pool" {
   cluster    = "wbaas-2"
   name       = "compute-pool"
