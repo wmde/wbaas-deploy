380281cf6b1e4ddd2f28175832bfec38f155aca3
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-04-12 13:18:33 +0000

    fix: tf linting error

--- tf/env/staging/secrets-tailscale.tf
+++ tf/env/staging/secrets-tailscale.tf
@@ -1,11 +1,11 @@
 resource "kubernetes_secret" "tailscale-secret" {
   metadata {
-    name = "operator-oauth"
+    name      = "operator-oauth"
     namespace = "tailscale"
   }
 
   data = {
-    client_id = var.tailscale_client_id
+    client_id     = var.tailscale_client_id
     client_secret = var.tailscale_client_secret
   }
 }
