7b7ababe52bac918d6e8d8b0291dbaeeebd50dab
Frederik Ring <frederik.ring@wikimedia.de>
2024-05-06 12:47:00 +0200

    chore(api/staging): rename oauth key to prepare for rotation (#1578)

--- tf/env/staging/secrets-api.tf
+++ tf/env/staging/secrets-api.tf
@@ -1,7 +1,12 @@
-resource "tls_private_key" "api-passport" {
+resource "tls_private_key" "api-passport-legacy" {
   # RSA 4096 per https://github.com/laravel/passport/blob/10.x/src/Console/KeysCommand.php#L45
   algorithm = "RSA"
   rsa_bits  = 4096
+}
+
+moved {
+  from = tls_private_key.api-passport
+  to   = tls_private_key.api-passport-legacy
 }
 
 resource "random_password" "api-app-key" {
