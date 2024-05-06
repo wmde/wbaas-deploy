231a0c9f389211d60ec8c4c19dc75be0332144f4
Frederik Ring <frederik.ring@wikimedia.de>
2024-05-06 12:52:10 +0200

    chore(api/staging): rotate oauth keys

--- tf/env/staging/secrets-api.tf
+++ tf/env/staging/secrets-api.tf
@@ -1,22 +1,23 @@
-resource "tls_private_key" "api-passport-legacy" {
+resource "tls_private_key" "api-passport" {
   # RSA 4096 per https://github.com/laravel/passport/blob/10.x/src/Console/KeysCommand.php#L45
   algorithm = "RSA"
   rsa_bits  = 4096
-}
-
-moved {
-  from = tls_private_key.api-passport
-  to   = tls_private_key.api-passport-legacy
 }
 
 resource "random_password" "api-app-key" {
   length           = 32
   special          = true
   override_special = "_%@"
+  keepers = {
+    rotate = "2"
+  }
 }
 
 resource "random_password" "api-app-jwt-secret" {
   length           = 32
   special          = true
   override_special = "_%@"
+  keepers = {
+    rotate = "2"
+  }
 }
