7dea20b6235dc0b29bcc324a93bc33ed63ccaf86
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-05-22 09:48:02 +0000

    tf: local path for monitoring module

--- tf/env/staging/monitoring.tf
+++ tf/env/staging/monitoring.tf
@@ -1,5 +1,5 @@
 module "staging-monitoring" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/monitoring?ref=tf-module-monitoring-26"
+  source = "../../modules/monitoring"
   providers = {
     kubernetes = kubernetes.wbaas-2
   }
