81c035045b0a2b850895a5d23a61b468c71a2e75
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-05-23 12:52:21 +0000

    tf: add shard alerts

--- tf/modules/monitoring/metric-alarms.tf
+++ tf/modules/monitoring/metric-alarms.tf
@@ -12,6 +12,36 @@ locals {
       min_group_by            = "metric.label.cluster"
     },
   }
+
+  elasticsearch = {
+    health = {
+      "yellow-${var.environment}" = {
+        display_name = "Elasticsearch Cluster Health Yellow"
+        color        = "yellow"
+        duration     = "900s"
+      },
+      "red-${var.environment}" = {
+        display_name = "Elasticsearch Cluster Health Red"
+        color        = "red"
+        duration     = "60s"
+      }
+    }
+
+    shards = {
+      "shard-count-${var.environment}" = {
+        display_name = "Elasticsearch Document Count"
+        metric       = "elasticsearch_indices_shards_docs"
+        # 75% of the 200M max documents per shard recommendation
+        threshold    = "150000000"
+      },
+      "shard-size-${var.environment}" = {
+        display_name = "Elasticsearch Shard Size"
+        metric       = "elasticsearch_indices_shards_store_size_in_bytes"
+        # 75% of the 50GB max shard size recommendation
+        threshold    = "37500000000"
+      }
+    }
+  }
 }
 
 resource "google_monitoring_alert_policy" "alert_policy_prometheus_metric" {
@@ -65,23 +95,8 @@ resource "google_monitoring_alert_policy" "alert_policy_prometheus_metric" {
   ]
 }
 
-locals {
-  elasticsearch = {
-    "es-cluster-yellow-${var.environment}" = {
-      display_name = "Elasticsearch Cluster Health Yellow"
-      color        = "yellow"
-      duration     = "900s"
-    },
-    "es-cluster-red-${var.environment}" = {
-      display_name = "Elasticsearch Cluster Health Red"
-      color        = "red"
-      duration     = "60s"
-    }
-  }
-}
-
 resource "google_monitoring_alert_policy" "alert_policy_prometheus_metric_elasticsearch_health" {
-  for_each = local.elasticsearch
+  for_each = local.elasticsearch.health
 
   display_name = "Metric check failed (${var.environment}): ${each.value.display_name}"
 
@@ -133,6 +148,49 @@ resource "google_monitoring_alert_policy" "alert_policy_prometheus_metric_elasti
         ]
         per_series_aligner = "ALIGN_MAX"
       }
+    }
+  }
+  combiner = "OR"
+  notification_channels = [
+    "${var.monitoring_email_group_name}"
+  ]
+}
+
+resource "google_monitoring_alert_policy" "alert_policy_prometheus_metric_elasticsearch_shards" {
+  for_each = local.elasticsearch.shards
+
+  display_name = "Metric check failed (${var.environment}): ${each.value.display_name}"
+
+  documentation {
+    content = "This alert fires if the metric ${each.value.display_name} does not meet its expected status."
+  }
+
+  conditions {
+    display_name = each.value.display_name
+    condition_threshold {
+      evaluation_missing_data = "EVALUATION_MISSING_DATA_ACTIVE"
+      comparison              = "COMPARISON_GT"
+      duration                = "300s"
+      filter = join(" AND ", [
+        "resource.type = \"prometheus_target\"",
+        "resource.labels.cluster = \"${var.cluster_name}\"",
+        "metric.type = \"prometheus.googleapis.com/${each.value.metric}/gauge\""
+      ])
+      trigger {
+        count = 1
+      }
+      threshold_value = each.value.threshold
+    }
+  }
+  conditions {
+    display_name = "${each.value.display_name} absent"
+    condition_absent {
+      duration = "300s"
+      filter = join(" AND ", [
+        "resource.type = \"prometheus_target\"",
+        "resource.labels.cluster = \"${var.cluster_name}\"",
+        "metric.type = \"prometheus.googleapis.com/${each.value.metric}/gauge\""
+      ])
     }
   }
   combiner = "OR"
