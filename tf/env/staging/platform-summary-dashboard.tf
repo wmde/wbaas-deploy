e315d8704e981783d8aeae6a82d76ffe0c0eeac0
Thomas Arrow <thomas.arrow@wikimedia.de>
2024-05-23 15:54:48 +0100

    fixup: TF linting

--- tf/env/staging/platform-summary-dashboard.tf
+++ tf/env/staging/platform-summary-dashboard.tf
@@ -1,6 +1,6 @@
 resource "google_monitoring_dashboard" "platform-summary" {
   dashboard_json = file("./platform-summary-dashboard-json.json")
-  project = "wikibase-cloud"
+  project        = "wikibase-cloud"
 
   timeouts {}
 }
