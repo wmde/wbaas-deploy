eae0fae0571a0f159599d00c41bba3acf5d6673c
Thomas Arrow <thomas.arrow@wikimedia.de>
2024-05-28 12:32:59 +0100

    fix: production TF remove monitoring param override
    This patch removes an override that prevents the default
    platform summary metrics from being using in the module

--- tf/env/production/monitoring.tf
+++ tf/env/production/monitoring.tf
@@ -6,21 +6,6 @@ module "production-monitoring" {
   environment                 = "production"
   cluster_name                = local.production_cluster_name
   monitoring_email_group_name = google_monitoring_notification_channel.monitoring_email_group.name
-  platform_summary_metrics = toset([
-    "active",
-    "total",
-    "deleted",
-    "inactive",
-    "empty",
-    "total_non_deleted_users",
-    "total_non_deleted_active_users",
-    "total_non_deleted_pages",
-    "total_non_deleted_edits",
-    "wikis_created_PT24H",
-    "wikis_created_P30D",
-    "users_created_PT24H",
-    "users_created_P30D",
-  ])
 }
 
 resource "google_logging_metric" "production-site-request-count" {
