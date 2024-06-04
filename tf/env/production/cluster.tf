cb74e58c36f11753b019ef177fd87fe371742b63
Andrew Kostka <andrew@minifile.com>
2024-06-04 10:27:36 +0200

    Revert "chore(tf): delete compute-pool-1 on production (#1627)"
    This reverts commit fb1acefa3a1374deff6c667be16fe44d2fb804e3.

--- tf/env/production/cluster.tf
+++ tf/env/production/cluster.tf
@@ -23,9 +23,9 @@ resource "google_container_cluster" "wbaas-3" {
 # Be careful altering node pools. If your change results in recreation then there will be a time
 # when there are no nodes in the pool. You may wish to create a new node pool first and then
 # delete the old one.
-resource "google_container_node_pool" "wbaas-3_compute-pool-2" {
+resource "google_container_node_pool" "wbaas-3_compute-pool-1" {
   cluster    = "wbaas-3"
-  name       = "compute-pool-2"
+  name       = "compute-pool-1"
   node_count = 3
   node_locations = [
     "europe-west3-a",
@@ -33,7 +33,7 @@ resource "google_container_node_pool" "wbaas-3_compute-pool-2" {
   node_config {
     disk_size_gb = 64
     disk_type    = "pd-ssd"
-    machine_type = "n2-highmem-16"
+    machine_type = "n2-standard-16"
     metadata = {
       "disable-legacy-endpoints" = "true"
     }
