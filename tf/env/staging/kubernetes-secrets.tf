a70eca4eaa048abf65df88168a02d9013748e63f
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-06-25 08:02:23 +0000

    chore(tf): create a modules-next copy of k8s-secrets

--- tf/env/staging/kubernetes-secrets.tf
+++ tf/env/staging/kubernetes-secrets.tf
@@ -1,5 +1,5 @@
 module "wbaas2-k8s-secrets" {
-  source = "git::ssh://git@github.com/wmde/wbaas-deploy//tf//modules/k8s-secrets?ref=tf-module-k8s-secrets-3"
+  source = "../../modules-next/k8s-secrets"
   providers = {
     kubernetes = kubernetes.wbaas-2
   }
