0d68eec832e97b079923a730eb39cb947bb97f44
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-06-26 14:28:10 +0000

    feat(superset): deploy a lightweight version on staging

---
+++ tf/env/local/secrets-superset.tf
@@ -0,0 +1,18 @@
+resource "random_password" "superset-passwords" {
+  for_each = toset(["secret", "admin-password"])
+  length           = 32
+  special          = true
+  override_special = "_%@"
+}
+
+resource "kubernetes_secret" "superset-secrets" {
+  metadata {
+    name      = "superset-secrets"
+    namespace = "default"
+  }
+
+  binary_data = {
+    "secret"         = base64encode(random_password.superset-passwords["secret"].result)
+    "admin-password" = base64encode(random_password.superset-passwords["admin-password"].result)
+  }
+}
