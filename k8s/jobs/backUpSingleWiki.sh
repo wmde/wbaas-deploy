98f8a83433d640d1043bcabf481e77ceda173c77
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2024-09-02 13:21:14 +0200

    pass DATABASE_NAME

--- k8s/jobs/backUpSingleWiki.sh
+++ k8s/jobs/backUpSingleWiki.sh
@@ -2,5 +2,8 @@
 
 # WBS_DOMAIN should be set to the domain to reindex
 
+kubectl apply -f backUpSingleWikiPvc.yaml
+
 kubectl create -f backUpSingleWiki.yaml -o=json --dry-run=client |\
+jq ".spec.template.spec.containers[0].env += [{\"name\": \"DATABASE_NAME\", \"value\": \"${DATABASE_NAME}\"}]" |\
 kubectl create -f -
