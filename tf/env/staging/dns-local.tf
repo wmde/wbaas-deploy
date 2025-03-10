d59c14eb24579d88fc876e8426c06e428a4ddb87
dena <dena@wikimedia.de>
2025-03-10 16:59:36 +0100

    add dns-local.tf

---
+++ tf/env/staging/dns-local.tf
@@ -0,0 +1,49 @@
+resource "google_dns_managed_zone" "local" {
+  description = "Zone for wbaas.dev"
+  dns_name    = "wbaas.dev."
+  name        = "wbaas-dev-local"
+  visibility  = "public"
+}
+
+resource "google_dns_record_set" "local-NS" {
+  managed_zone = google_dns_managed_zone.local.name
+  name         = google_dns_managed_zone.local.dns_name
+  rrdatas = [
+    "ns-cloud-b1.googledomains.com.",
+    "ns-cloud-b2.googledomains.com.",
+    "ns-cloud-b3.googledomains.com.",
+    "ns-cloud-b4.googledomains.com.",
+  ]
+  ttl  = 21600
+  type = "NS"
+}
+
+resource "google_dns_record_set" "local-SOA" {
+  managed_zone = google_dns_managed_zone.local.name
+  name         = google_dns_managed_zone.local.dns_name
+  rrdatas = [
+    "ns-cloud-b1.googledomains.com. cloud-dns-hostmaster.google.com. 1 21600 3600 259200 300",
+  ]
+  ttl  = 21600
+  type = "SOA"
+}
+
+resource "google_dns_record_set" "local-A" {
+  managed_zone = google_dns_managed_zone.local.name
+  name         = google_dns_managed_zone.local.dns_name
+  rrdatas = [
+    "127.0.0.1"
+  ]
+  ttl  = 300
+  type = "A"
+}
+
+resource "google_dns_record_set" "local-wildcard-A" {
+  managed_zone = google_dns_managed_zone.local.name
+  name         = "*.wbaas.dev."
+  rrdatas = [
+    "127.0.0.1"
+  ]
+  ttl  = 300
+  type = "A"
+}
