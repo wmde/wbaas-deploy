9491ab76519b2a5f29263d27532eef284e860a79
dena <dena@wikimedia.de>
2025-03-10 17:56:00 +0100

    update configuration, taking staging as an example

--- tf/env/staging/dns-local.tf
+++ tf/env/staging/dns-local.tf
@@ -1,18 +1,37 @@
 resource "google_dns_managed_zone" "local" {
-  description = "Zone for wbaas.dev"
+  description = "DNS zone for domain: wbaas.dev"
   dns_name    = "wbaas.dev."
   name        = "wbaas-dev-local"
   visibility  = "public"
+
+  dnssec_config {
+    kind          = "dns#managedZoneDnsSecConfig"
+    non_existence = "nsec3"
+    state         = "on"
+
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 2048
+      key_type   = "keySigning"
+      kind       = "dns#dnsKeySpec"
+    }
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 1024
+      key_type   = "zoneSigning"
+      kind       = "dns#dnsKeySpec"
+    }
+  }
 }
 
 resource "google_dns_record_set" "local-NS" {
   managed_zone = google_dns_managed_zone.local.name
   name         = google_dns_managed_zone.local.dns_name
   rrdatas = [
-    "ns-cloud-b1.googledomains.com.",
-    "ns-cloud-b2.googledomains.com.",
-    "ns-cloud-b3.googledomains.com.",
-    "ns-cloud-b4.googledomains.com.",
+    "ns-cloud-d1.googledomains.com.",
+    "ns-cloud-d2.googledomains.com.",
+    "ns-cloud-d3.googledomains.com.",
+    "ns-cloud-d4.googledomains.com.",
   ]
   ttl  = 21600
   type = "NS"
@@ -22,7 +41,7 @@ resource "google_dns_record_set" "local-SOA" {
   managed_zone = google_dns_managed_zone.local.name
   name         = google_dns_managed_zone.local.dns_name
   rrdatas = [
-    "ns-cloud-b1.googledomains.com. cloud-dns-hostmaster.google.com. 1 21600 3600 259200 300",
+    "ns-cloud-d1.googledomains.com. cloud-dns-hostmaster.google.com. 3 21600 3600 259200 300",
   ]
   ttl  = 21600
   type = "SOA"
