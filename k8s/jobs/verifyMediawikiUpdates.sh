eb53a96b7ba2d2d8bd667c0b15eaffb8c64f79b8
dena <91744937+deer-wmde@users.noreply.github.com>
2025-12-04 13:53:01 +0100

    add mediawiki update job verification script (#2371)
    * add mediawiki update job verification script
    
    * decrease caching TTL in nginx conf and add a delay before removing the read-only mode of a wiki (#2370)
    
    * Fix broken curl command (#2372)
    
    * (chore): Update api-queue CPU requests (#2342)
    
    * (chore): Update api-queue CPU requests
    
    * Update api.values.yaml.gotmpl
    
    * argo values
    
    * Staging+Local: Deploy new MediaWiki image ghcr.io/wbstack/mediawiki:sha-a72fadf (#2373)
    
    * Production: Deploy new MediaWiki image ghcr.io/wbstack/mediawiki:sha-a72fadf (#2374)
    
    * improve output
    
    * update README
    
    * clean up output
    
    * fix typo
    
    ---------
    
    Co-authored-by: Perside Rosalie <perside.rosalie@wikimedia.de>
    Co-authored-by: wmdebot <121924840+wmdebot@users.noreply.github.com>

---
+++ k8s/jobs/verifyMediawikiUpdates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+JOBS=$(kubectl get jobs -n adhoc-jobs -o json)
+
+COMPLETED=$(echo $JOBS | jq -r '.items[] | select((.status.succeeded // 0) >= 1 and (.metadata.labels | has("wikiDomain"))) | "\(.metadata.name)\t\(.metadata.labels.wikiDomain)"')
+
+INCOMPLETE=$(echo $JOBS | jq -r '.items[] | select((.status.succeeded // 0) < 1 and (.metadata.labels | has("wikiDomain"))) | "\(.metadata.name)\t\(.metadata.labels.wikiDomain)"')
+# alternative query for incomplete jobs without wikiDomain label
+#INCOMPLETE=$(echo $JOBS | jq -r '.items[] | select((.status.succeeded // 0) < 1) | "\(.metadata.name)"')
+
+COMPLETED_COUNT=$(printf "%s" "$COMPLETED" | wc -l)
+INCOMPLETE_COUNT=$(printf "%s" "$INCOMPLETE" | wc -l)
+
+echo "Completed jobs:"
+echo "$COMPLETED"
+echo 
+echo "Number of completed jobs: $COMPLETED_COUNT"
+echo
+echo '---'
+echo
+echo "Incomplete jobs:"
+echo "$INCOMPLETE"
+echo
+echo "Number of completed jobs: $INCOMPLETE_COUNT"
