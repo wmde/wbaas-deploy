2b0167d56aa7e3741a52587b745733cf7124b241
Thomas Arrow <tarrow@users.noreply.github.com>
2026-01-15 10:57:15 +0000

    prod(tf): create k8s secret for botstopper imagePull (#2436)
    Defines a secret to be used to pull the bot stopper image.
    Specifically this defines a Docker config Secret[0]
    
    This was originally created following the instructions from anubis[1]
    and then the encoded content extracted by me.
    
    Like our other tf secrets this needs to be stored by the deploying
    engineer in tf/env/staging/terraform.tfvars
    
    Once this commit has been deployed other engineers do not need to set
    this variable in terraform.tfvars due to the default. The reason this
    works still slightly eludes me but it seems to be a feature of how
    having a default set at all prevents prompting[2] but also the change
    detection logic only changing if there is a provided new value rather
    than the empty string which is treated as an unprovided value.
    
    [0] https://kubernetes.io/docs/concepts/configuration/secret/#docker-config-secrets
    [1] https://anubis.techaro.lol/docs/admin/botstopper/#kubernetes
    [2] https://developer.hashicorp.com/terraform/language/values/variables#reference-variable-values
    
    Bug: [T402205](https://phabricator.wikimedia.org/T402205)

---
+++ tf/env/production/secrets-botstopper.tf
@@ -0,0 +1,24 @@
+# Generate from kubectl create secret docker-registry \
+# techarohq-botstopper \
+# --dry-run=client \
+# --docker-server ghcr.io \
+# --docker-username placeholder-username \
+# --docker-password <your-access-token> \
+
+# Where <your-access-token> is a GitHub classic token which has read access to the botstopper repo
+# n.b. in Jan 2026 the username is not checked by github hence the placeholder
+resource "kubernetes_secret" "botstopper-image-pull" {
+
+  provider = kubernetes.wbaas-3
+
+  metadata {
+    namespace = "default"
+    name      = "botstopper-image-pull"
+  }
+
+  data = {
+    ".dockerconfigjson" = var.botstopper_image_pull_json_secret,
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
