66af320f17021893a1967c45ab4c3e765ef414bd
dena <91744937+deer-wmde@users.noreply.github.com>
2026-02-11 15:39:23 +0100

    fix: local wiki script (#2576)
    * adjust for https
    * use cookies instead of token
    * include survey data
    
    Bug: T416720
    Co-authored-by: Ollie <oliver.hyde@wikimedia.de>
    Co-authored-by: Dat WMDE <dat.nguyen@wikimedia.de>

--- bin/local/create-local-wiki.sh
+++ bin/local/create-local-wiki.sh
@@ -22,28 +22,42 @@ USER_WIKI_NAME="${USER_WIKI_NAME:-Local Test Wiki}"
 USER_WIKI_DOMAIN="${USER_WIKI_DOMAIN:-local-test-wiki.wbaas.dev}"
 USER_WIKI_ADMIN="${USER_WIKI_ADMIN:-Admin}"
 
-TEST_RESPONSE=$(curl -s http://api.wbaas.dev/healthz)
+API_URL="https://www.wbaas.dev/api"
+
+## Setup
+COOKIE_JAR="${XDG_STATE_HOME:-$HOME/.local/state}/wbaas.dev/cookies.txt"
+
+mkdir -p "$(dirname "${COOKIE_JAR}")"
+
+## Check if API is reachable
+TEST_RESPONSE=$(curl --insecure --location --silent "${API_URL}/healthz")
 if [[ "${TEST_RESPONSE}" != "It's Alive" ]]; then
     echo "Error: local api is not available. Is the minikube tunnel open? ('make minikube-tunnel')"
     exit 2
 fi
 
+## Obtain Session cookie
 LOGIN_JSON_DATA=$(jo email="${USER_MAIL}" password="${USER_PASS}")
-LOGIN_RESPONSE=$(curl -s 'http://api.wbaas.dev/auth/login' \
+LOGIN_RESPONSE=$(curl --insecure --location --silent --cookie-jar "${COOKIE_JAR}" "${API_URL}/auth/login" \
     -X POST \
     -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
     --data-raw "${LOGIN_JSON_DATA}")
 
-LOGIN_TOKEN=$(echo "${LOGIN_RESPONSE}" | jq -r '.token')
-[[ -n "$LOGIN_TOKEN" ]] || { echo "error: login failed." 1>&2; exit 1; }
+LOGIN_MAIL=$(echo "${LOGIN_RESPONSE}" | jq -r '.user.email')
+[[ "$LOGIN_MAIL" == "$USER_MAIL" ]] || { echo "error: login failed." 1>&2; exit 1; }
 
-CREATE_WIKI_JSON_DATA=$(jo domain="${USER_WIKI_DOMAIN}" sitename="${USER_WIKI_NAME}" username="${USER_WIKI_ADMIN}")
-CREATE_WIKI_RESPONSE=$(curl -s 'http://api.wbaas.dev/wiki/create' \
+## Create wiki
+CREATE_WIKI_JSON_DATA=$(jo -- \
+    domain="${USER_WIKI_DOMAIN}" \
+    sitename="${USER_WIKI_NAME}" \
+    username="${USER_WIKI_ADMIN}" \
+    -s profile="$(jo purpose=decide_later temporality=decide_later)")
+CREATE_WIKI_RESPONSE=$(curl --insecure --location --silent "${API_URL}/wiki/create" \
     -X POST \
     -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
-    -H "Authorization: Bearer ${LOGIN_TOKEN}" \
+    --cookie "${COOKIE_JAR}" \
     --data-raw "${CREATE_WIKI_JSON_DATA}")
 
 echo "$CREATE_WIKI_RESPONSE"
